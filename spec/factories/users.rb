44f3c5efd09a8faefcaec318b0f315fe3987d6d9
masaki-engineer <gp77.am@gmail.com>
2022-02-08 22:36:54 +0900

    FactoryBotのパスワードの生成方法の変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,13 @@
 FactoryBot.define do
   factory :user do
-
-    person = Gimei.name
-
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}
-    last_name             {person.last.kanji}
-    first_name            {person.first.kanji}
-    last_name_kana        {person.last.katakana}
-    first_name_kana       {person.first.katakana}
+    last_name             {'真崎'}
+    first_name            {'彰大'}
+    last_name_kana        {'マサキ'}
+    first_name_kana       {'アキヒロ'}
     birth_date            {Faker::Date.backward}
   end
 end
