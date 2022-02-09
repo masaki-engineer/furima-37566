63b4e4a2973ba54fd5cac718271f9bd551209aa4
masaki-engineer <gp77.am@gmail.com>
2022-02-09 15:29:00 +0900

    バリデーションの設定（image以外）

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,13 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
+
+  validates :name,                   presence: true
+  validates :info,                   presence: true
+  validates :category_id,            numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sales_status_id,        numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id,          numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_delivery_id,  numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price,                  presence: true, numericality: { in: 300..9999999 }
 end
