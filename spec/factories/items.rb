9d514e3a7a1d95b605ef90be70e1f1b748ab8cf7
masaki-engineer <gp77.am@gmail.com>
2022-02-09 17:55:54 +0900

    FactoryBoxnの設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name                   { Faker::Lorem.sentence }
+    info                   { Faker::Lorem.sentence }
+    category_id            { Random.rand(2..11) }
+    sales_status_id        { Random.rand(2..7) }
+    shipping_fee_status_id { Random.rand(2..3) }
+    prefecture_id          { Random.rand(2..48) }
+    scheduled_delivery_id  { Random.rand(2..4) }
+    price                  { Random.rand(300..9999999) }
+    association :user
   end
 end
