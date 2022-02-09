49bdb643f945594b07a854620f58bb228422628b
masaki-engineer <gp77.am@gmail.com>
2022-02-09 14:53:58 +0900

    itemモデルとActiveHashのモデルのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
 end
