7e0283fe16fda532cec271b10f2f6847532fa098
masaki-engineer <gp77.am@gmail.com>
2022-02-11 16:03:12 +0900

    newアクションの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,12 @@
 class OrdersController < ApplicationController
 
-  def new
+  def index
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    binding.pry
   end
-  
+
 end
