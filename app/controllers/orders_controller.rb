e54ba4e0b4db420f900a135d271102c19c055cc3
masaki-engineer <gp77.am@gmail.com>
2022-02-12 16:28:09 +0900

    条件分岐（売却済商品の購入画面に進めない）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,9 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
+    if @item.order != nil
+      redirect_to root_path
+    end
   end
 
   def create
